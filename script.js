0972e685e8c54074bd1acc97e030edf1087b02f2
Ajinkyap22 <palaskarap15@gmail.com>
2021-02-10 15:11:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Create an initial grid container with 16 x 16 square divs
+
+// append the grids to a container
+
+// Grid must change color each time you hover over them
+
+// Grids should become 10% less visible on each hover
+
+// Add a button on top which clears all the current grids & prompts the user for new grid size
+
+// New grid should take same space as old one
